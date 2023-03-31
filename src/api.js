d95ee22b8ebc4b2533b01675c3e6660312e4ee98
BruhNav <pranav20100@iiitnr.edu.in>
2023-03-31 10:13:08 +0530

    finished final version

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e99d83cd33msh0f68ee8daaac3e8p1ae33fjsn958180a21a77",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d79d610bfddc7f4b60ba90a8753755d7"; 
